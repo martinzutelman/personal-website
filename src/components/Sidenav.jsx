a4352928d1a547bfa35f11f53ff247b38865e002
martinzutelman <zutelmanmartin@gmail.com>
2023-06-22 22:22:49 -0300

    added an alert button with an href to reddit, jut to check if the icons and links are working

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { AiOutlineHome, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineAlert, AiOutlineHome, AiOutlineMenu } from 'react-icons/ai'
 import './Sidenav.css'
 import Button1 from './button'
 const Sidenav = () => { 
@@ -16,6 +16,8 @@ const Sidenav = () => {
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
                     <Button1 Icon={AiOutlineHome} Text='Home' Href="https://twitter.com/?lang=es"/>
                     <Button1 Icon={AiOutlineMenu} Text='Menu' Href="https://www.youtube.com/"/>
+                    <Button1 Icon={AiOutlineAlert} Text='Alert' Href="https://www.reddit.com"/>
+
                 </div>
             )
             : (
